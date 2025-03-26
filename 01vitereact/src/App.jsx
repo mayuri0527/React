94c6442c13e0b54a4cafc48a1a966039dda0c9d0
mayuri0527 <mj052827@gmail.com>
2025-03-26 10:11:03 +0530

    01basicreact

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,14 @@
-
+import Chai from "./chai"
 
 function App() {
   
 
   return (
-    <h1>React with vite | MJ</h1>
+    <>
+    <Chai/>
+    <h1>chai aur react</h1>
+    <p>test para</p>
+    </>
   )
 }
 
