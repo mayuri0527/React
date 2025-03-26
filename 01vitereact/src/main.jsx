48cf06a329ccf890f3bece33ba3e81c48febaf63
mayuri0527 <mj052827@gmail.com>
2025-03-27 00:22:26 +0530

    01vitereact

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,30 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App !</h1>
+        </div>
+    )
+}
+
+const anotherElement = ( 
+    <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+const anotherUser = " chai aur react "
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'click me to visit google',
+    anotherElement
+)
+
 createRoot(document.getElementById('root')).render(
  
-    <App />
+ reactElement
 
 )
